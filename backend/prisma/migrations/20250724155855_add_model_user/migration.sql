e3532e5ce6d3cee3feb356c0a436e1edc9dd1474
aerelish <elisharagas@gmail.com>
2025-07-25 00:03:41 +0800

    adding User model for db schema

---
+++ backend/prisma/migrations/20250724155855_add_model_user/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "isActive" BOOLEAN NOT NULL DEFAULT true,
+    "isVerified" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
--- backend/src/routes/registerRoute.ts
+++ backend/src/routes/registerRoute.ts
@@ -9,7 +9,7 @@ const router = express.Router();
 router.post('/', async (req, res) => {
 
   const { 
-    username, 
+    username,
     password
   } = req.body;
  
